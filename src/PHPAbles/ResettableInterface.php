268b67538f5f3ac4e4c7d2da6b9bb42d5a96deae
Jay Potter <j@ypotter.ca>
2024-11-03 16:32:19 -0500

    extend Symfony\Contracts\Service\ResetInterface

--- src/PHPAbles/ResettableInterface.php
+++ src/PHPAbles/ResettableInterface.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 namespace PHPAbles;
 
-use Monolog\ResettableInterface as BaseResettableInterface;
+use Monolog\ResettableInterface as MonologResettable,
+    Symfony\Contracts\Service\ResetInterface as SymfonyResettable;
 
-interface ResettableInterface extends BaseResettableInterface
+interface ResettableInterface extends MonologResettable, SymfonyResettable
 {
     public function reset(): void;
 }
